54f48e9b7b21cfe68f25e592c514f3910d68e182
maxwell <246339@edu.p.lodz.pl>
2025-12-10 18:32:43 +0100

    ADD:info about developer 246339

--- arithmetics/src/main/java/com/ejaj/Main.java
+++ arithmetics/src/main/java/com/ejaj/Main.java
@@ -2,6 +2,8 @@ package com.ejaj;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ej-aj_Masters \t Developer \t MarkdeBrunner");
+        System.out.println("ej-aj_Masters \n " +
+				"Developer \t MarkdeBrunner\n" +
+				"Developer \t 246339" +);
     }
 }
