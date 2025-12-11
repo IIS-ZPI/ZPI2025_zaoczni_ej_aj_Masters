3439fb56a231d6f7d349e1db30a61af6f0bb36b3
krzysztofwieczorkowski <wieczorkowski.k7@gmail.com>
2025-12-11 16:06:28 +0100

    added printing role and GitHub Name

--- arithmetics/src/main/java/com/ejaj/Main.java
+++ arithmetics/src/main/java/com/ejaj/Main.java
@@ -2,6 +2,6 @@ package com.ejaj;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ej-aj_Masters \t Developer \t MarkdeBrunner");
+        System.out.println("ej-aj_Masters \t DevOps \t krzysztofwieczorkowski");
     }
 }
