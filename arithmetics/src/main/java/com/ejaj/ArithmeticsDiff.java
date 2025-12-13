2ec03a7ab124e9745e44cd380e676d3706c8a68d
krzysztofwieczorkowski <wieczorkowski.k7@gmail.com>
2025-12-13 23:44:08 +0100

    Commented ArithmeticsDiff.Difference

--- arithmetics/src/main/java/com/ejaj/ArithmeticsDiff.java
+++ arithmetics/src/main/java/com/ejaj/ArithmeticsDiff.java
@@ -1,6 +1,7 @@
 package com.ejaj;
 
 public class ArithmeticsDiff implements IArithmeticsDiff {
+    //method to subtract 2 values
     @Override
     public double Difference(double A, double B) {
         return A - B;
