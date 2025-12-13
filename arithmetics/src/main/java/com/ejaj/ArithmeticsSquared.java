1108d0209f9c6559384daf14aba24c63927c2083
MarkdeBrunner <246325@edu.p.lodz.pl>
2025-12-13 23:05:53 +0100

    Revert "Refactor Squared method in ArithmeticsSquared to use ArithmeticsMult for multiplication"
    This reverts commit d5f1550dc7405bdf6cf8b5c03c831a71ce52bfd8.

--- arithmetics/src/main/java/com/ejaj/ArithmeticsSquared.java
+++ arithmetics/src/main/java/com/ejaj/ArithmeticsSquared.java
@@ -3,7 +3,6 @@ package com.ejaj;
 public class ArithmeticsSquared implements IArithmeticsSquared {
     @Override
     public double Squared(double A) {
-        ArithmeticsMult multiplicator = new ArithmeticsMult();
-        return multiplicator.Multiplication(A, A);
+        return A * A;
     }
 }
