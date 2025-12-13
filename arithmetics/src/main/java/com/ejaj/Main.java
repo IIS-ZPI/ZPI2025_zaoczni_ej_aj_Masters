bccb26eb544af44ddae88e5d2f671d0b3038ca5d
MarkdeBrunner <246325@edu.p.lodz.pl>
2025-12-13 22:33:12 +0100

    Update Main class comment for clarity

--- arithmetics/src/main/java/com/ejaj/Main.java
+++ arithmetics/src/main/java/com/ejaj/Main.java
@@ -1,9 +1,9 @@
 package com.ejaj;
-
+// Main class to display team information
 public class Main {
     public static void main(String[] args) {
         System.out.println("ej-aj_Masters \n" +
-				"Developer \tMarkdeBrunner\n" +
+				"Developer \tMarkdeBrunner\n" + 
 				"\t246339\n" +
         		"\tmic-cza\n" +
                 "\tkrzysztofwieczorkowski");
