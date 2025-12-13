7f20dc1c80358d1007234587883f8a9534e3531d
mic-cza <czarneckimichal92@gmail.com>
2025-12-13 21:54:28 +0100

    Added missing space im ArithmeticsDiv

--- arithmetics/src/main/java/com/ejaj/ArithmeticsDiv.java
+++ arithmetics/src/main/java/com/ejaj/ArithmeticsDiv.java
@@ -1,6 +1,6 @@
 package com.ejaj;
 
-public class ArithmeticsDiv implements IArithmeticsDiv{
+public class ArithmeticsDiv implements IArithmeticsDiv {
     @Override
     public double Division(double A, double B) {
         return A / B;
