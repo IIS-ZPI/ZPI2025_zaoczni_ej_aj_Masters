ebae779ca456fea4b94de254338c6bd4ffccb4d8
mic-cza <czarneckimichal92@gmail.com>
2025-12-14 12:14:18 +0100

    Added comment

--- arithmetics/src/main/java/com/ejaj/Main.java
+++ arithmetics/src/main/java/com/ejaj/Main.java
@@ -3,6 +3,7 @@ package com.ejaj;
 public class Main {
     //Function prints GitHub names of our team
     public static void main(String[] args) {
+        // team
         System.out.println("ej-aj_Masters \n" +
 				"Developer \tMarkdeBrunner\n" +
 				"\t246339\n" +
