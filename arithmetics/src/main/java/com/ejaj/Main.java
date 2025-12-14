079ecc58617faa6177d71c16719e0c35b08190eb
maxwell <246339@edu.p.lodz.pl>
2025-12-14 11:46:13 +0100

    ADD: comment in Main

--- arithmetics/src/main/java/com/ejaj/Main.java
+++ arithmetics/src/main/java/com/ejaj/Main.java
@@ -2,6 +2,7 @@ package com.ejaj;
 
 public class Main {
     //Function prints GitHub names of our team
+	//Funkcja wypisuje nazwy GitHub naszych członków zespołu
     public static void main(String[] args) {
         System.out.println("ej-aj_Masters \n" +
 				"Developer \tMarkdeBrunner\n" +
