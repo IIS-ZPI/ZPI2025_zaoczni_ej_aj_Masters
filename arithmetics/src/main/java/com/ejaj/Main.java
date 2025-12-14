203aacc9589ea94bfc4316a1536fc2d8c2e38c9f
krzysztofwieczorkowski <wieczorkowski.k7@gmail.com>
2025-12-14 10:31:38 +0100

    Added description to main function

--- arithmetics/src/main/java/com/ejaj/Main.java
+++ arithmetics/src/main/java/com/ejaj/Main.java
@@ -1,6 +1,7 @@
 package com.ejaj;
 
 public class Main {
+    //Function prints GitHub names of our team
     public static void main(String[] args) {
         System.out.println("ej-aj_Masters \n" +
 				"Developer \tMarkdeBrunner\n" +
